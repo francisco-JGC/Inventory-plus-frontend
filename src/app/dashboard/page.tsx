96773d0c6c899fb1e7ab2f50b96969be24db6ed0
francisco-JGC <jjjchico1@gmail.com>
2024-08-30 01:08:02 -0600

    chore: Add dashboard page component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      dqwdwqd
+    </div>
+  );
+}
