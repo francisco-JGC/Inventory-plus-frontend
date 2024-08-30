a346448395f288f001191598dd6a82e89cf9a29d
francisco-JGC <jjjchico1@gmail.com>
2024-08-30 01:09:14 -0600

    feat: Add MENU_ITEMS constant for dashboard navigation

---
+++ src/constant/MENU_ITEMS.ts
@@ -0,0 +1,36 @@
+import path from "path";
+import {
+  LayoutDashboard,
+  BoxesIcon,
+  Users,
+  FileBadge,
+  ReceiptIcon,
+} from "lucide-react";
+
+export const MENU_ITEMS = [
+  {
+    icon: LayoutDashboard,
+    label: "Panel",
+    path: "/dashboard",
+  },
+  {
+    icon: ReceiptIcon,
+    label: "Facturación",
+    path: "/dashboard/billing",
+  },
+  {
+    icon: BoxesIcon,
+    label: "Inventario",
+    path: "/dashboard/inventory",
+  },
+  {
+    icon: Users,
+    label: "Usuarios",
+    path: "/dashboard/users",
+  },
+  {
+    icon: FileBadge,
+    label: "Reportes",
+    path: "/dashboard/reports",
+  },
+];
