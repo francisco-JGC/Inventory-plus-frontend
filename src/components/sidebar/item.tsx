4e92c25ac5159d033bc9b27cfb4dff625e93a182
francisco-JGC <jjjchico1@gmail.com>
2024-09-02 23:12:51 -0600

    feat: linking menu items

--- src/components/sidebar/item.tsx
+++ src/components/sidebar/item.tsx
@@ -1,26 +1,34 @@
 'use client'
 import { useContext } from "react";
 import { SidebarContext } from ".";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 interface ISidebarItemProps {
   icon: React.ReactNode;
   text: string;
-  active?: boolean;
   alert?: boolean;
+  path: string
 }
 
-export const SidebarItem = ({ icon, text, active, alert }: ISidebarItemProps) => {
+export const SidebarItem = ({ icon, text, alert, path }: ISidebarItemProps) => {
+  const pathname = usePathname()
+  const isActive = (path: string) => path === pathname;
+
   const { expanded } = useContext(SidebarContext);
   return (
-    <li className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group
-      ${active
-        ? "bg-gradient-to-tr from-indigo-200 to indigo-100 text-indigo-800"
+    <Link href={path} className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group
+      ${isActive(path)
+        ? "bg-indigo-50 text-indigo-600"
         : "hover:bg-indigo-50 text-gray-600"}`}
     >
       {icon}
+
       <span className={`overflow-hidden transition-all
-        ${expanded ? "w-52 ml-3" : "w-0"}`
-      }>{text}</span>
+        ${expanded ? "w-52 ml-3" : "w-0"}`}
+      >
+        {text}
+      </span>
 
       {alert && <div className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${expanded ? "" : "top-2"}`} />}
 
@@ -34,6 +42,6 @@ export const SidebarItem = ({ icon, text, active, alert }: ISidebarItemProps) =>
           </div>
         )
       }
-    </li>
+    </Link>
   )
 }
