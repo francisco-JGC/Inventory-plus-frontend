420af6413b9ae8a25e8ead926bddc0beb32c4bb4
francisco-JGC <jjjchico1@gmail.com>
2024-09-03 23:33:15 -0600

    feat: create actions buttons component

---
+++ src/app/dashboard/_components/actionsLinks.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button"
+import { Box, Boxes, File, Plus, Users } from "lucide-react"
+import Link from "next/link"
+
+export const ActionsLinks = () => {
+  return (
+    <div className="bg-white shadow-sm rounded-lg w-full p-4 ">
+      <div>
+        <span className='p-4 text-gray-400 font-semibold text-xs tracking-wider'>ACCIONES RAPIDAS</span>
+      </div>
+      <div className="flex gap-4 justify-between p-4">
+        <Button variant={"secondary"}>
+          <Link href={""} className="flex gap-2 items-center">
+            <Plus width={17} />
+            Crear Producto
+          </Link>
+        </Button>
+
+        <Button variant={"secondary"}>
+          <Link href={""} className="flex gap-2 items-center">
+            <Users width={17} />
+            Administrar Usuarios
+          </Link>
+        </Button>
+
+        <Button variant={"secondary"}>
+          <Link href={""} className="flex gap-2 items-center">
+            <Box width={17} />
+            Registrar Nueva Venta
+          </Link>
+        </Button>
+
+        <Button variant={"secondary"}>
+          <Link href={""} className="flex gap-2 items-center">
+            <Boxes width={17} />
+            Ver Inventario
+          </Link>
+        </Button>
+
+        <Button variant={"secondary"}>
+          <Link href={""} className="flex gap-2 items-center">
+            <File width={17} />
+            Generar Reportes
+          </Link>
+        </Button>
+      </div>
+    </div>
+  )
+}
