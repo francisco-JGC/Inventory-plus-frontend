9f2c827c2dcfd7a0a43fce9eff2274cd69191c45
francisco-JGC <jjjchico1@gmail.com>
2024-09-03 22:53:52 -0600

    feat: render fluctuationChart component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { StatisticData } from "./_components/statisticData";
+import { FluctuationChart } from "./_components/chartFluctuation";
 
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex flex-col gap-4">
       <StatisticData />
+      <FluctuationChart />
     </div>
   );
 }
