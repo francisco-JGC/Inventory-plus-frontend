ca9699ec6b05066a8f21ee9ba158b3efecbdefba
francisco-JGC <jjjchico1@gmail.com>
2024-09-03 23:46:06 -0600

    fix: render new options

--- src/app/dashboard/_components/statisticData.tsx
+++ src/app/dashboard/_components/statisticData.tsx
@@ -1,10 +1,21 @@
+import { Button } from "@/components/ui/button"
 import { SeparatorLine, StatisticDataItem } from "./StatisticDataItem"
+import { Download, FileArchiveIcon, FileBadge } from "lucide-react"
 
 export const StatisticData = () => {
   return (
     <div className="bg-white shadow-sm rounded-lg w-full p-4 ">
-      <div>
-        <span className='p-4 text-gray-400 font-semibold text-xs tracking-wider'>VISTA GENERAL</span>
+      <div className="flex justify-between gap-4">
+        <div className="flex flex-col px-4">
+          <span className='text-gray-400 font-semibold text-xs tracking-wider'>VISTA GENERAL</span>
+          <small className="text-gray-400">Información del mes actual</small>
+        </div>
+        <div>
+          <Button className="flex gap-2 items-center bg-indigo-500 hover:bg-indigo-600/90" title="Exportar Registro de venta del Mes">
+            <Download width={17} />
+            Exportar
+          </Button>
+        </div>
       </div>
       <div className='flex justify-between relative'>
         <StatisticDataItem
@@ -31,7 +42,7 @@ export const StatisticData = () => {
           increase={0}
         />
       </div>
-    </div>
+    </div >
   )
 }
 
