d7989ffe55ac6471d1bcd5b30a3d36deb462502d
francisco-JGC <jjjchico1@gmail.com>
2024-09-03 23:33:06 -0600

    feat: render actions buttons

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,14 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { StatisticData } from "./_components/statisticData";
 import { FluctuationChart } from "./_components/chartFluctuation";
+import { ActionsLinks } from "./_components/actionsLinks";
 
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-4">
       <StatisticData />
+      <ActionsLinks />
       <FluctuationChart />
     </div>
   );
