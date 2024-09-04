ba7c9c3a00705fd359d576fceb6af23885c87482
francisco-JGC <jjjchico1@gmail.com>
2024-09-03 22:54:07 -0600

    feat: create Fluctuation Chart

---
+++ src/app/dashboard/_components/chartFluctuation.tsx
@@ -0,0 +1,128 @@
+"use client"
+
+import * as React from "react"
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+
+const chartData = [
+  { date: "2024-1", sales: 222, },
+  { date: "2024-2", sales: 97, },
+  { date: "2024-3", sales: 167, },
+  { date: "2024-4", sales: 242, },
+  { date: "2024-5", sales: 373, },
+  { date: "2024-6", sales: 301, },
+]
+
+const chartConfig = {
+  views: {
+    label: "Total",
+  },
+  sales: {
+    label: "Ventas",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig
+
+export function FluctuationChart() {
+  const [activeChart, setActiveChart] =
+    React.useState<keyof typeof chartConfig>("sales")
+
+  const total = React.useMemo(
+    () => ({
+      sales: chartData.reduce((acc, curr) => acc + curr.sales, 0),
+    }),
+    []
+  )
+
+  return (
+    <Card>
+      <CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row">
+        <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6">
+          <CardTitle>Fluctuación</CardTitle>
+          <CardDescription>
+            Registros de ventas en los ultimos 6 meses
+          </CardDescription>
+        </div>
+        <div className="flex">
+          {["sales"].map((key) => {
+            const chart = key as keyof typeof chartConfig
+            return (
+              <button
+                key={chart}
+                data-active={activeChart === chart}
+                className="relative z-30 flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l data-[active=true]:bg-muted/50 sm:border-l sm:border-t-0 sm:px-8 sm:py-6"
+                onClick={() => setActiveChart(chart)}
+              >
+                <span className="text-xs text-muted-foreground">
+                  {chartConfig[chart].label}
+                </span>
+                <span className="text-lg font-bold leading-none sm:text-3xl">
+                  {total[key as keyof typeof total].toLocaleString()}
+                </span>
+              </button>
+            )
+          })}
+        </div>
+      </CardHeader>
+      <CardContent className="px-2 sm:p-6">
+        <ChartContainer
+          config={chartConfig}
+          className="aspect-auto h-[250px] w-full"
+        >
+          <BarChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="date"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              minTickGap={32}
+              tickFormatter={(value) => {
+                const date = new Date(value)
+                return date.toLocaleDateString("es-ES", {
+                  month: "long",
+                })
+              }}
+            />
+            <ChartTooltip
+              content={
+                <ChartTooltipContent
+                  className="w-[150px]"
+                  nameKey="views"
+                  labelFormatter={(value) => {
+                    return new Date(value).toLocaleDateString("en-US", {
+                      month: "short",
+                      day: "numeric",
+                      year: "numeric",
+                    })
+                  }}
+                />
+              }
+            />
+            <Bar dataKey={activeChart} fill={`var(--color-${activeChart})`} />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  )
+}
