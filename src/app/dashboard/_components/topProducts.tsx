1bcb9a656018a50e2f5c0ebc2d8a5ea88a382fc5
francisco-JGC <jjjchico1@gmail.com>
2024-09-04 00:00:36 -0600

    feat: create top productos component

---
+++ src/app/dashboard/_components/topProducts.tsx
@@ -0,0 +1,87 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const invoices = [
+  {
+    product_name: "Nombre del product 1",
+    provider: "Paid",
+    sales: "710",
+    stock: "Credit Card",
+  },
+  {
+    product_name: "Nombre del product 2",
+    provider: "Pending",
+    sales: "490",
+    stock: "PayPal",
+  },
+  {
+    product_name: "Nombre del product 3",
+    provider: "Unpaid",
+    sales: "902",
+    stock: "Bank Transfer",
+  },
+  {
+    product_name: "Nombre del product 4",
+    provider: "Paid",
+    sales: "1002",
+    stock: "Credit Card",
+  },
+  {
+    product_name: "Nombre del product 5",
+    provider: "Paid",
+    sales: "687",
+    stock: "PayPal",
+  },
+  {
+    product_name: "Nombre del product 6",
+    provider: "Pending",
+    sales: "892",
+    stock: "Bank Transfer",
+  },
+  {
+    product_name: "Nombre del product 7",
+    provider: "Unpaid",
+    sales: "672",
+    stock: "Credit Card",
+  },
+]
+
+export const TopProducts = () => {
+  return (
+    <div className="bg-white p-6">
+      <div>
+        <h3 className="text-lg font-bold">PRODUCTOS TOP</h3>
+      </div>
+
+      <Table>
+        <TableCaption>Una lista de los productos mas vendido</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[300px]">Nombre del producto</TableHead>
+            <TableHead>Proveedor</TableHead>
+            <TableHead>Stock</TableHead>
+            <TableHead className="text-right">Ventas</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoices) => (
+            <TableRow key={invoices.product_name}>
+              <TableCell className="font-medium">{invoices.product_name}</TableCell>
+              <TableCell>{invoices.provider}</TableCell>
+              <TableCell>{invoices.stock}</TableCell>
+              <TableCell className="text-right">{invoices.sales}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
