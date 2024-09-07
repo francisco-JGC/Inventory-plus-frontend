02d2f11de2f89db725e7be9e099a4b173c40b091
francisco-JGC <jjjchico1@gmail.com>
2024-09-07 17:06:01 -0600

    fix: styles

--- src/app/dashboard/_components/StatisticDataItem.tsx
+++ src/app/dashboard/_components/StatisticDataItem.tsx
@@ -4,13 +4,15 @@ interface IStatisticItemsProps {
   label: string
   value: string
   increase: number
+  titleStyles?: string
+  valueStyles?: string
 }
 
-export const StatisticDataItem = ({ value, label, increase }: IStatisticItemsProps) => {
+export const StatisticDataItem = ({ value, label, increase, titleStyles, valueStyles }: IStatisticItemsProps) => {
   return (
-    <div className="p-4 rounded-sm flex flex-col gap-4">
+    <div className="p-4 rounded-sm flex flex-col gap-4 ">
       <div className='flex items-center gap-4'>
-        <h3 className='text-2xl font-semibold text-gray-800 text-center w-full'>{value}</h3>
+        <h3 className={`text-2xl font-semibold text-gray-800 text-center w-full ${titleStyles}`}>{value}</h3>
         <div className='flex gap-1 items-center'>
           {
             increase !== 0 && (
@@ -22,7 +24,7 @@ export const StatisticDataItem = ({ value, label, increase }: IStatisticItemsPro
           }
         </div>
       </div>
-      <span className='text-gray-500 text-sm'>{label}</span>
+      <span className={`text-gray-500 text-sm ${valueStyles}`}>{label}</span>
     </div>
   )
 }
