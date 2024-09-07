b7cbec6dcb7f5f5c59ea2484ab91bb2907365d8b
francisco-JGC <jjjchico1@gmail.com>
2024-09-07 17:06:23 -0600

    feat: render list product

--- src/app/dashboard/inventory/page.tsx
+++ src/app/dashboard/inventory/page.tsx
@@ -1,12 +1,17 @@
+import { InventoryListProduct } from "./_components/inventoryListProduct";
 import { InventoryValue } from "./_components/inventoryValue";
 import { StatisticData } from "./_components/statisticData";
 
 export default function InventoryPage() {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <div className="flex justify-between gap-4">
         <StatisticData />
         <InventoryValue />
+      </div>
+
+      <div>
+        <InventoryListProduct />
       </div>
     </div>
   );
