ba4de581ebef427c0b054cd45b234c8e9cf06e49
francisco-JGC <jjjchico1@gmail.com>
2024-09-07 18:25:02 -0600

    feat: create columns for data table

---
+++ src/app/dashboard/inventory/_components/listProductColumns.tsx
@@ -0,0 +1,94 @@
+import { Button } from "@/components/ui/button"
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { IProduct } from "./inventoryListProduct"
+import { DateFormat } from "@/utils/date-format"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+
+export const ColumnsListProduct: ColumnDef<IProduct>[] = [
+  {
+    accessorKey: "product_name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Nombre del producto
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+    cell: ({ row }) => <div className="lowercase">{row.getValue("product_name")}</div>,
+  },
+  {
+    accessorKey: "stock",
+    header: "Stock",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("stock")}</div>
+    ),
+  },
+  {
+    accessorKey: "low_stock_limit",
+    header: "Limite de Stock Bajo",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("low_stock_limit")}</div>
+    ),
+  },
+  {
+    accessorKey: "status",
+    header: "Estado",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("status")}</div>
+    ),
+  },
+  {
+    accessorKey: "updated_at",
+    header: "Ultima Modificación",
+    cell: ({ row }) => {
+      return <div className="capitalize">{DateFormat(row.getValue("updated_at"))}</div>
+    },
+  },
+  {
+    accessorKey: "price",
+    header: () => <div className="text-right">Precio</div>,
+    cell: ({ row }) => {
+      const stock = parseFloat(row.getValue("price"))
+
+      // Format the stock as a dollar stock
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(stock)
+
+      return <div className="text-right font-medium">{formatted}</div>
+    },
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const product = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Modificar Información</DropdownMenuItem>
+            <DropdownMenuItem>Ver detalles de producto</DropdownMenuItem>
+            <DropdownMenuItem>Abastecer Stock</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="text-red-400">Eliminar Producto</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },
+]
