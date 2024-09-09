1ee968f85895c3001b0e0d61a4e1a46d3509659a
francisco-JGC <jjjchico1@gmail.com>
2024-09-09 14:59:16 -0600

    feat: create users page

---
+++ src/app/dashboard/users/page.tsx
@@ -0,0 +1,9 @@
+import { UsersList } from "./_components/usersList";
+
+export default function UsersPage() {
+  return (
+    <div className="flex flex-col gap-4">
+      <UsersList />
+    </div>
+  );
+}
