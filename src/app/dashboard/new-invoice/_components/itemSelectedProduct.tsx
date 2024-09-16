82437bdf009ccf2b9230b7cd193ba924c14ab34a
francisco-JGC <jjjchico1@gmail.com>
2024-09-16 15:32:23 -0600

    feat: styles bug

--- src/app/dashboard/new-invoice/_components/itemSelectedProduct.tsx
+++ src/app/dashboard/new-invoice/_components/itemSelectedProduct.tsx
@@ -12,17 +12,16 @@ interface IProps {
 
 export const ItemSelectedProduct = ({ product, handleItemQuantity, handleDeleteSelectedProduct }: IProps) => {
   return (
-    <div className="w-full border p-2 rounded-lg flex gap-4 items-center">
-      <span className="w-2/4 font-bold">{product.product_name}</span>
+    <div className="w-full border p-2 rounded-lg font-bold grid grid-cols-5 gap-4">
+      <span className="font-bold col-span-2">{product.product_name}</span>
       <Input
         value={product.quantity}
         type="number"
         onChange={(e) => handleItemQuantity(product.id, Number(e.currentTarget.value))}
         min={1}
         max={product.stock}
-        className="w-2/4"
       />
-      <span className="w-2/2">
+      <span>
         {PriceFormat(product.price)}
       </span>
       <Button
