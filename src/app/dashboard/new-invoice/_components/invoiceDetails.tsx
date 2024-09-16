a8ca1e5e57203f038feeede30fb39b36337d48fd
francisco-JGC <jjjchico1@gmail.com>
2024-09-16 15:31:43 -0600

    fix: styles bug

--- src/app/dashboard/new-invoice/_components/invoiceDetails.tsx
+++ src/app/dashboard/new-invoice/_components/invoiceDetails.tsx
@@ -17,7 +17,7 @@ interface IProps {
 
 export const InvoiceDetails = ({ products, handleInputChangeClientInfo, clientInfo, selectedProducts, handleSelectedProduct, handleItemQuantity, handleDeleteSelectedProduct }: IProps) => {
   return (
-    <div className="flex flex-col gap-4 w-full">
+    <div className="flex flex-col gap-4 w-2/4">
       <h3 className="font-bold">Detalles de Factura</h3>
       <div className="flex flex-col gap-2">
         <label htmlFor="clientName">Nombre del cliente <span className="text-gray-400">{'(opcional)'}</span></label>
@@ -49,29 +49,32 @@ export const InvoiceDetails = ({ products, handleInputChangeClientInfo, clientIn
             products={products}
           />
         </div>
-        <div className="flex flex-col gap-2 max-w-[450px]">
-          <div className="w-full flex justify-between gap-4 mb-2">
-            <span className="font-bold text-gray-600 w-2/4">Item</span>
-            <span className="font-bold text-gray-600 w-2/4">Cantidad</span>
-            <span className="font-bold text-gray-600 w-2/4">Precio U.</span>
+        <div className="flex flex-col gap-2 w-full">
+          <div className="w-full mb-2 font-bold grid grid-cols-5 gap-4">
+            <span className="font-bold text-gray-600 col-span-2">Item</span>
+            <span className="font-bold text-gray-600">Cantidad</span>
+            <span className="font-bold text-gray-600">Precio U.</span>
+
           </div>
-          {
-            selectedProducts.length > 0 ? (
-              selectedProducts.map((product) => (
-                <ItemSelectedProduct
-                  product={product}
-                  handleItemQuantity={handleItemQuantity}
-                  key={product.id}
-                  handleDeleteSelectedProduct={handleDeleteSelectedProduct}
-                />
-              ))
-            )
-              : (
-                <div>
-                  <span className="text-gray-400">No se han seleccionado productos</span>
-                </div>
+          <div className="max-h-60 min-h-60 overflow-auto flex flex-col gap-2">
+            {
+              selectedProducts.length > 0 ? (
+                selectedProducts.map((product) => (
+                  <ItemSelectedProduct
+                    product={product}
+                    handleItemQuantity={handleItemQuantity}
+                    key={product.id}
+                    handleDeleteSelectedProduct={handleDeleteSelectedProduct}
+                  />
+                ))
               )
-          }
+                : (
+                  <div className="w-full flex items-center justify-center">
+                    <span className="text-gray-400">No se han seleccionado productos</span>
+                  </div>
+                )
+            }
+          </div>
         </div>
       </div>
     </div >
