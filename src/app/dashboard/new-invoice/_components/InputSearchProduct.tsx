bdfa5dfac6043db953a948c19a940633b84136ff
francisco-JGC <jjjchico1@gmail.com>
2024-09-16 11:33:58 -0600

    feat: create search product component

---
+++ src/app/dashboard/new-invoice/_components/InputSearchProduct.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import * as React from "react"
+import { Check, ChevronsUpDown } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { useState } from "react"
+import { IProductInvoice, ISelectedItem, ISelectedProducts } from "../page"
+
+interface IProps {
+  selectedProducts: ISelectedItem[]
+  handleSelectedProduct: (product: ISelectedItem) => void
+  products: IProductInvoice[]
+}
+
+export function InputSearchProduct({ selectedProducts, products, handleSelectedProduct }: IProps) {
+  const [open, setOpen] = useState(false)
+  const [value, setValue] = useState("")
+
+  return (
+    <div className="w-full">
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-full justify-between"
+          >
+            Seleccione un producto...
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-full p-0">
+          <Command>
+            <CommandInput placeholder="Buscar producto..." />
+            <CommandList>
+              <CommandEmpty>Sin resultados.</CommandEmpty>
+              <CommandGroup>
+                {products.map((item) => (
+                  <CommandItem
+                    key={item.id}
+                    value={item.id.toString()}
+                    onSelect={() => {
+                      handleSelectedProduct({ ...item, quantity: 1 })
+                      setOpen(false)
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        selectedProducts.find((product) => product.id === item.id)?.id
+                          ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                    {item.product_name}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </div>
+  )
+}
