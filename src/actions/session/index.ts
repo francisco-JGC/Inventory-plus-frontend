1657859406031a9cd3bde513ea509aad3ddea316
francisco-JGC <jjjchico1@gmail.com>
2024-10-01 20:38:45 -0600

    feat: create session actions

--- src/actions/session/index.ts
+++ src/actions/session/index.ts
@@ -1,3 +1,52 @@
-export const getSession = () => {};
-export const login = () => {};
-export const logout = () => {};
+import { fetchData } from '@/utils/fetch-data'
+import { toast } from 'sonner'
+import { setCookie } from 'cookies-next'
+
+export const login = async (
+  email: string,
+  password: string,
+): Promise<boolean> => {
+  try {
+    const response = await fetchData<any>({
+      url: '/auth/login',
+      method: 'POST',
+      data: { email, password },
+    })
+
+    toast.dismiss()
+
+    if (!response.success) {
+      toast.error('Error al iniciar sesión', {
+        description: response.message,
+      })
+      return false
+    }
+
+    toast.success('Inicio de sesión exitoso!')
+    setCookie('token', response.data.token)
+    return true
+  } catch (error) {
+    toast.error('Error al iniciar sesión', {
+      description: 'revisa las credenciales',
+    })
+    return false
+  }
+}
+
+export const logout = (): void => {
+  localStorage.removeItem('authToken')
+  localStorage.removeItem('user')
+}
+
+export const getSession = () => {
+  const token = localStorage.getItem('authToken')
+  if (!token) {
+    return null
+  }
+
+  const user = localStorage.getItem('user')
+  return {
+    token,
+    user: user ? JSON.parse(user) : null,
+  }
+}
