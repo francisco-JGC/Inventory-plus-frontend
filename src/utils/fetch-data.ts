d4936271acf0f7e3ad42fe7beef43597dfe02374
francisco-JGC <jjjchico1@gmail.com>
2024-10-01 20:39:50 -0600

    feat: take token to fetch data

--- src/utils/fetch-data.ts
+++ src/utils/fetch-data.ts
@@ -1,49 +1,49 @@
+import { getCookie } from 'cookies-next'
+
 interface FetchDataParams<T> {
-  url: string;
-  method: "GET" | "POST" | "PUT" | "DELETE";
-  data?: T;
-  useToken?: boolean;
+  url: string
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE'
+  data?: T
+  useToken?: boolean
 }
 
-async function fetchData<TResponse, TData = any>({
+export async function fetchData<TResponse, TData = any>({
   url,
   method,
   data,
   useToken = false,
 }: FetchDataParams<TData>): Promise<TResponse> {
   const headers: HeadersInit = {
-    "Content-Type": "application/json",
-  };
+    'Content-Type': 'application/json',
+  }
 
   if (useToken) {
-    const token = localStorage.getItem("authToken");
+    const token = getCookie('token')
     if (token) {
-      headers["Authorization"] = `Bearer ${token}`;
+      headers['Authorization'] = `Bearer ${token}`
     } else {
-      throw new Error("Token no encontrado");
+      throw new Error('Token no encontrado')
     }
   }
 
   const options: RequestInit = {
     method,
     headers,
-  };
+  }
 
-  if (method !== "GET" && data) {
-    options.body = JSON.stringify(data);
+  if (method !== 'GET' && data) {
+    options.body = JSON.stringify(data)
   }
 
   try {
-    const response = await fetch(url, options);
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}${url}`,
+      options,
+    )
 
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status} - ${response.statusText}`);
-    }
-
-    const result: TResponse = await response.json();
-    return result;
+    return await response.json()
   } catch (error) {
-    console.error("Error en la solicitud:", error);
-    throw error;
+    console.error('Error en la solicitud:', error)
+    throw error
   }
 }
